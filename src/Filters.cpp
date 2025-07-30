139796945cb8fc58643f009bd3f34523ab92f925
David-Salihu <davidsalihu19@gmail.com>
2025-07-30 07:46:58 +0100

    Refactor convertToGrayscale to handle different channel sizes

--- src/Filters.cpp
+++ src/Filters.cpp
@@ -6,8 +6,17 @@
 // Convert image int rgb to grayscale using luminosity method
 Image convertToGrayscale(Image &image)
 {
-
     int totalBytes = image.width * image.height;
+    int no_of_channels = 1;
+    if (image.no_of_chnls <= 2) // Already in grayscale
+    {
+        return image;
+    }
+    else if (image.no_of_chnls == 4)
+    {
+        totalBytes *= 2; // Expand totalBytes to include alpha channel (2 channels total)
+        ++no_of_channels;
+    }
     std::vector<unsigned char> grayscale_pixels;
     int counter = 0;
     for (int i = 0; i < image.pixels.size(); i++)
@@ -15,20 +24,23 @@ Image convertToGrayscale(Image &image)
         uint8_t red = image.pixels[i];
         uint8_t green = image.pixels[++i];
         uint8_t blue = image.pixels[++i];
-
         // Equation for converstion to grayscale
         float pixel_value = 0.3 * red + 0.59 * green + 0.11 * blue;
         grayscale_pixels.push_back(pixel_value);
-    }
 
+        if (image.no_of_chnls == 4) // If there are 4 channels, add the Alpha channel
+        {
+            grayscale_pixels.push_back(image.pixels[++i]);
+        }
+    }
     if (!(totalBytes == grayscale_pixels.size()))
     {
         std::cout << "Error converting Image!" << std::endl;
-        return 0;
+        return Image();
     }
 
-    // Create grayscale image
-    Image grayscale_image(image.width, image.height, GRAY_SCALE_CHNL_SIZE, image.max_val);
+    // Create grayscale image object
+    Image grayscale_image(image.width, image.height, no_of_channels, image.max_val);
     grayscale_image.pixels = grayscale_pixels;
 
     return grayscale_image;
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main()
     std::cout << "Mini Image Processing Library!" << std::endl;
     FileIO file_io;
 
-    Image image = file_io.loadImage("../assets/image.png");
+    Image image = file_io.loadImage("../assets/rgba_image.png");
     Image grayscale_image = convertToGrayscale(image);
 
     file_io.saveImage(grayscale_image, "../assets/image_out.png");
